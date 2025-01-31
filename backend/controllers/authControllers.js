a884b8574fee0bf7d552389d69331eb506f1dfe6
RyneeeJ <imrynegandia@gmail.com>
2025-01-31 22:14:06 +0800

    Implement protectRoute controller

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -22,31 +22,44 @@ const signCookieToken = (id, res) => {
   return token;
 };
 
+exports.protectRoute = function ({ isSigningUp = false }) {
+  return async (req, res, next) => {
+    try {
+      // retrieve jwt from cookie or auth headers
+      const token =
+        req.cookies?.jwt || req.headers.authorization?.replace("Bearer ", "");
+
+      if (isSigningUp && !token) return next();
+
+      // validate jwt
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // check if user still exists
+      const user = await User.findById(decoded.id).select("role");
+      if (!user)
+        throw new AppError(
+          "The owner of this token does not exist. Please log in again.",
+          401
+        );
+      // check if user has changed his password after issuing the jwt
+      if (user.passwordChangedAfterTokenIssued(decoded.iat))
+        throw new AppError(
+          "The user changed password after this token was issued. Please log in again.",
+          401
+        );
+
+      // if valid, set user to req.user
+      req.user = user;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
 exports.checkAdminRole = async (req, res, next) => {
   try {
-    // retrieve jwt from cookie or auth headers
-    const token =
-      req.cookies?.jwt || req.headers.authorization?.replace("Bearer ", "");
-
-    if (!token) return next();
-    // validate jwt
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // check if user still exists
-    const user = await User.findById(decoded.id).select("role");
-    if (!user)
-      throw new AppError(
-        "The owner of this token does not exist. Please log in again.",
-        401
-      );
-    // check if user has changed his password after issuing the jwt
-    if (user.passwordChangedAfterTokenIssued(decoded.iat))
-      throw new AppError(
-        "The user changed password after this token was issued. Please log in again.",
-        401
-      );
-    // if valid, set 'admin' role to req.role
-    if (user.role === "admin") req.role = "admin";
+    if (req.user && req.user.role === "admin") req.role = "admin";
     next();
   } catch (err) {
     next(err);
@@ -104,6 +117,17 @@ exports.login = async (req, res, next) => {
       status: "Success",
       token,
       message: "Logged in successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.logout = (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: "Success",
+      message: "Logged out successfully",
     });
   } catch (err) {
     next(err);
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,12 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authControllers");
 
-router.post("/signup", authController.checkAdminRole, authController.signup);
+router.post(
+  "/signup",
+  authController.protectRoute({ isSigningUp: true }),
+  authController.checkAdminRole,
+  authController.signup
+);
 router.post("/login", authController.login);
-
+router.post("logout", authController.protectRoute, authController.logout);
 module.exports = router;
