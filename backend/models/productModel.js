18fb6d066ef18204c4c78ed4778567a4f7fb51b1
RyneeeJ <imrynegandia@gmail.com>
2025-02-02 07:47:34 +0800

    Add variant schema for product variants, modify productSchema to include variantSchema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,34 +1,68 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Product name is required"],
-  },
-  image: {
-    type: String,
-    required: [true, "Please provide product image"],
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide product description"],
-  },
-  stocks: {
+const variantSchema = new mongoose.Schema({
+  color: String,
+  size: String,
+  variantImage: String,
+  stock: {
     type: Number,
     min: 0,
-    required: [true, "Please list number of stocks for this product"],
+    required: [true, "Products must have number of stocks"],
   },
-  ratingsAverage: {
-    type: Number,
-    min: 1,
-    max: 5,
-    default: 4.5,
+  price: {
+    type: String,
+    required: [true, "Please provide a price for your product/s"],
   },
-  isFeatured: {
-    type: Boolean,
-    default: false,
+  SKU: {
+    type: String,
+    required: [true, "Each product variant must have a unique SKU"],
+    unique: true,
   },
 });
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    primaryImage: {
+      type: String,
+      required: [true, "Please provide product image"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide product description"],
+    },
+    variants: {
+      type: [variantSchema],
+      required: [true, "A product must have at least one variant"],
+      validate: {
+        validator(variants) {
+          return variants.length > 0;
+        },
+        message: "At least one variant is required",
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 4.5,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Implement possible variants of the same product
+// * Products could or could not have variants
+// * Variants of the same products could have the same or different prices
+// * Products with only one variant/type have only one price and one 'stock'
+// * Products with multiple variants have different number of stocks
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
